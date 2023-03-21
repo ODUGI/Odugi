d81fbcb553bab73992e07872398aa195e9ebab56
nno3onn <nno3onn@naver.com>
2023-03-21 15:23:11 +0900

    refac: Common 컴포넌트에서 라우팅 처리 코드 제거

--- src/pages/Common.tsx
+++ src/pages/Common.tsx
@@ -6,8 +6,8 @@ import InviteFriendModal from "@components/organisms/Modal/InviteFriendModal";
 import UserSettingModal from "@components/organisms/Modal/UserSettingModal";
 import CommonPage from "@components/templates/CommonPage";
 import useModalStore from "@store/useModalStore";
-import { useCallback, useEffect } from "react";
-import { useMatch, useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const modalTable = {
   inviteFriend: <InviteFriendModal />,
@@ -18,7 +18,6 @@ const modalTable = {
 
 const Common = () => {
   const navigate = useNavigate();
-  // const isBaseUrl = useMatch("/");
   //!TODO: channelId는 추후에 channelName을 받아오는 용도로 사용
   const { communityId, channelId } = useParams();
   const { setShowModal, showModal, modalType } = useModalStore();
@@ -29,11 +28,6 @@ const Common = () => {
   useEffect(() => {
     setShowModal(false);
   }, []);
-
-  // if (isBaseUrl) {
-  //   navigate("/@me");
-  //   return null;
-  // }
 
   const component = modalType ? modalTable[modalType] : null;
 
