758468d2e6a5cddbceedd970a1860175180f2c43
krokerdile <crazykroker@gmail.com>
2023-04-02 13:55:58 +0900

    feat: 카테고리 dnd 적용

--- src/components/organisms/Tab2CommunityBody.tsx
+++ src/components/organisms/Tab2CommunityBody.tsx
@@ -10,6 +10,8 @@ import AddIcon from "@components/atoms/Icons/AddIcon";
 import useModalStore from "@store/useModalStore";
 import useGetCommunityList from "@hooks/query/useGetCommunityList";
 import useGetCategoryList from "@hooks/query/useGetCategoryList";
+import { useState } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 interface ChannelType {
   channel_id: number;
@@ -29,10 +31,13 @@ const Tab2CommunityBody = () => {
   const channelList = useGetChannelList({
     communityId,
   });
-  console.log(channelList);
   const categoryList = useGetCategoryList({
     communityId,
   });
+
+  const [dndCategoryList, setDndCategoryList] =
+    useState<ChannelType[]>(categoryList);
+  const [dndChannelList, setDndChannelList] = useState<RoomType[]>(channelList);
   const { userInfo } = useUserStore();
   const { data: friendList } = useGetFriendList(userInfo.email);
 
@@ -47,6 +52,17 @@ const Tab2CommunityBody = () => {
     setModalType("createChannel");
     setShowModal(true);
   };
+
+  const showPatchCategoryModal = () => {
+    setModalType("patchCategory");
+    setShowModal(true);
+  };
+  const showDeleteCategoryModal = () => {
+    setModalType("deleteCategory");
+    setShowModal(true);
+  };
+
+  console.log(categoryList);
 
   if (!communityId)
     return (
@@ -95,6 +111,14 @@ const Tab2CommunityBody = () => {
   //   navigate(`/${channelId}/${id}`);
   // }
 
+  const handleChange = (result: any) => {
+    if (!result.destination) return;
+    const items = [...dndCategoryList];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setDndCategoryList(items);
+  };
+
   return (
     <div>
       <button onClick={showCreateChannelModal}>
@@ -104,30 +128,48 @@ const Tab2CommunityBody = () => {
         <AddIcon />
       </button>
       {/* category = channel / room -> channel */}
-      {categoryList.map((category: any) => (
-        <>
-          <CommunityLabel text={category.name} />
-          {channelList
-            .filter((channel: any) => category.id === channel.categoryId)
-            .map((channel: any) => (
-              <>
-                <CommunityRoomButton
-                  type={channel.type === 1 ? "VOICE" : "CHAT"}
-                  text={channel.name}
-                  communityId={communityId}
-                  channelId={channel.id}
-                />
-                {channel.id === Number(channelId) && <UserChannelOnBox />}
-                {friendList.map((friend: FriendType) => (
-                  <UserFriendChannelOnBox
-                    friend={friend}
-                    channelId={channel["channel_id"]}
-                  />
-                ))}
-              </>
-            ))}
-        </>
-      ))}
+      <DragDropContext onDragEnd={handleChange}>
+        <Droppable droppableId="communities">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {categoryList.map((category: any) => (
+                <>
+                  <CommunityLabel text={category.name} />
+                  {channelList
+                    .filter(
+                      (channel: any) => category.id === channel.categoryId
+                    )
+                    .map((channel: any) => (
+                      <>
+                        <CommunityRoomButton
+                          type={channel.type === 1 ? "VOICE" : "CHAT"}
+                          text={channel.name}
+                          communityId={communityId}
+                          channelId={channel.id}
+                        />
+                        {channel.id === Number(channelId) && (
+                          <UserChannelOnBox />
+                        )}
+                        {friendList.map((friend: FriendType) => (
+                          <UserFriendChannelOnBox
+                            friend={friend}
+                            channelId={channel["channel_id"]}
+                          />
+                        ))}
+                      </>
+                    ))}
+                  <button onClick={showPatchCategoryModal}>
+                    <AddIcon />
+                  </button>
+                  <button onClick={showDeleteCategoryModal}>
+                    <AddIcon />
+                  </button>
+                </>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
